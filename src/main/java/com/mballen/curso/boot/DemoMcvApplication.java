4af3d4bc4ba514836c1b48e2c47847cd21716b1f
Jefferson Alves <jefff9910@outlook.com>
2023-05-13 15:48:18 -0300

    Iniciando projeto

---
+++ src/main/java/com/mballen/curso/boot/DemoMcvApplication.java
@@ -0,0 +1,13 @@
+package com.mballen.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMcvApplication.class, args);
+	}
+
+}
